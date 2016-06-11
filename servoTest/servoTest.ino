b00220e20b2b58dd9ab73f2460a3af895e78cb29
Alex <bockx082@umn.edu>
2016-06-11 18:27:26 -0500

    PID control implemented

--- servoTest/servoTest.ino
+++ servoTest/servoTest.ino
@@ -9,29 +9,38 @@ float z =  0.04;
 float gyro_x = 0.001;       // gyroscope
 float gyro_y = 0.060;
 float gyro_z = 0.001;
-float moi = 0.0075;         // kg*m^2
 float vel = 10.1;           // m/s
 // new variables after this point
-
-int numFins=2;
-float finArea = .0096;      // m^2
-float I = 0.0075;           // kg*m^2
-
 
 float roll = gyro_y;        // assumed axis of rotation (from last flight)
 float rollTarget = 0;       // desired angular rotation
 float rollTol = 0.00;
-float rollDif;
-float Kp = 800;
-float Ki = 0;
-float Kd = 0;
-float gyro_inc=-.002;
+float rollProp;
+float rollInt;
+float rollDer;
+float Kp = 200;
+float Ki = 6000;
+float Kd = -0.12;
 
-//Offsets to make servos align vertically at exactly v=90
+// sample data
+float gyro_10= .010;
+float gyro_9 = .010;
+float gyro_8 = -.030;
+float gyro_7 = -.030;
+float gyro_6 = -.030;
+float gyro_5 = .000;
+float gyro_4 = .000;
+float gyro_3 = .050;
+float gyro_2 = .050;
+float gyro_1 = .050;
+float gyro_0 = .000;
+float dataTime = .001;      // time between data (assuming interrupts)
+
+// Offsets to make servos align vertically at exactly v=90
 int servo1Offset = 8;
 int servo2Offset = 8;
 int v = 90;
-int vMax=30;
+int vMax = 12;              // max angular deflection (avoids stall)
 
 
 void setup() {
@@ -47,28 +56,55 @@ void setup() {
 
 
 void loop() {
-  roll = gyro_y;
-  rollDif = roll - rollTarget;
+  // Proportional
+  //roll = gyro_y;
+  roll = (gyro_1 + gyro_2)/2;   // averaging helpful for instantaneous errors
+  rollProp = roll - rollTarget;
+
+  // Integrated
+  rollInt = (gyro_1+gyro_2+gyro_3+gyro_4+gyro_5 - rollTarget*5) * dataTime*5;
+
+  // Derivative
+  rollDer = (gyro_1 - gyro_3)/(dataTime*3);  // just enough to average instantaneous errors
   
-  if (abs(rollDif)>rollTol) {
-        v=90 + Kp*rollDif;
+  if (abs(rollProp)>=rollTol) {
+        v = 90 + Kp*rollProp + Ki*rollInt + Kd*rollDer;
+        
         if (v>90+vMax) {
-            v=90+vMax;
+            v = 90 + vMax;
         }
         else if (v<90-vMax) {
-            v=90-vMax;
-          }
+            v = 90 - vMax;
+        }
         servo1.write(v + servo1Offset);
         servo2.write(v + servo2Offset);
+        
+
+        Serial.print  ("  90");
+        Serial.print  ("  +  ");
+        Serial.print  (Kp*rollProp);
+        Serial.print  ("  +  ");
+        Serial.print  (Ki*rollInt);
+        Serial.print  ("  +  ");
+        Serial.print  (Kd*rollDer);
+        Serial.print  ("  =  ");
+        Serial.println(v);
   }
+
+  // simulates changing gyro data - cycles through positions
+  gyro_0  = gyro_10;    // temporary storage variable
+  gyro_10 = gyro_9;
+  gyro_9 = gyro_8;
+  gyro_8 = gyro_7;
+  gyro_7 = gyro_6;
+  gyro_6 = gyro_5;
+  gyro_5 = gyro_4;
+  gyro_4 = gyro_3;
+  gyro_3 = gyro_2;
+  gyro_2 = gyro_1;
+  gyro_1 = gyro_0;
+  delay(300);
+
   
-  // simulates effect of stabilizing
-  if (gyro_y+gyro_inc < -.06  ||  gyro_y+gyro_inc > .06) {
-    gyro_inc=-gyro_inc;
-  }
-  else {
-    gyro_y = gyro_y + gyro_inc;
-  }
-  
-  delay(20);           // simulate time spent running other code
+  delay(20);           // simulates time spent running other code
 }
