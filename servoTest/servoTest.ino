43f3f9cac160c4ab9b72f2a5e97b5ba33485ad76
Alex <bockx082@umn.edu>
2016-06-30 22:02:35 -0500

    Now runs on IMU data

--- servoTest/servoTest.ino
+++ servoTest/servoTest.ino
@@ -1,74 +1,101 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 #include<Servo.h>
-#include<Wire.h>
 Servo servo1;
 Servo servo2;
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+//File myFile;
 
-// Remove these for use in code where the variables already exist
-float x =  0.06;            // accelerometer
-float y = -0.97;
-float z =  0.04;
-float gyro_x = 0.001;       // gyroscope
-float gyro_y = 0.060;
-float gyro_z = 0.001;
-float vel = 10.1;           // m/s
-// new variables after this point
+float Kp = 3;
+float Ki = 1.5;
+float Kd = -0.1;
 
-float roll = gyro_y;        // assumed axis of rotation (from last flight)
+float gyro[101];            // 1 larger than intended buffer size (invalid data bug)
+int i=0;
+int b;  int n;  int m;  int j;  int k;  int l;
+float loopDelay = 200;
+float dataTime = .204;      // time between data - approximated using millis()
+
+float roll;                 // assumed axis of rotation (from last flight)
 float rollTarget = 0;       // desired angular rotation
 float rollTol = 0.00;
 float rollProp;
 float rollInt;
 float rollDer;
-float Kp = 200;
-float Ki = 6000;
-float Kd = -0.12;
-
-// sample data
-float gyro_10= .010;
-float gyro_9 = .010;
-float gyro_8 = -.030;
-float gyro_7 = -.030;
-float gyro_6 = -.030;
-float gyro_5 = .000;
-float gyro_4 = .000;
-float gyro_3 = .070;
-float gyro_2 = .070;
-float gyro_1 = .070;
-float gyro_0 = .000;
-float dataTime = .001;      // time between data (assuming interrupts)
 
 // Offsets to make servos align vertically at exactly v=90
-int servo1Offset = 4;       // 4 for MG995
-int servo2Offset = 0;       // 0 for MG995
+int servo1Offset = 4;       // 4 for MG995 #1
+int servo2Offset = 0;       // 0 for MG995 #2
 int v = 90;
 int vMax = 12;              // max angular deflection (avoids stall)
 
 
 void setup() {
   pinMode(1,OUTPUT);
-  servo1.attach(9);         // analog pin 0
-  servo2.attach(10);
-  Serial.begin(19200);
-  Serial.println("  Ready");
-    Serial.print("  v = ");
-    Serial.println(v);
+  servo1.attach(3);
+  servo2.attach(2);
+  Serial.begin(9600);
+  if (!bno.begin()) {
+    Serial.print("No BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+
+  /*Serial.print("Initializing SD card ... ");
+  if (!SD.begin(10)) {
+    Serial.println("failed!");
+    return;
+  }
+  myFile = SD.open("testing.txt", FILE_WRITE);
+  Serial.println("done!");*/
+  
+  Serial.println("\tReady");
+  Serial.print("  v = ");
+  Serial.println(v);
   servo1.write(v + servo1Offset);
   servo2.write(v + servo2Offset);
-  delay(1000);              // remove for final use
+  delay(1000);
 }
 
+long timestep;
+long time0 = millis();
 
 void loop() {
+  i++;
+  if (i>100)  { i = 1; }
+  
+  imu::Vector<3> gyroscope = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  gyro[i] = gyroscope.x();      //may need to change to y or z dep. on orientation
+  
+  Serial.print("\t\t\t\t\t\t\tX:  ");
+  Serial.print(gyro[i]);
+  Serial.println("");
+  
+  // Fixes reference errors with circular buffer
+  b = i - 1;
+    if (b<1)  { b = b+100; }
+  n = i - 2;
+    if (n<1)  { n = n+100; }
+  j = i - 3;
+    if (j<1)  { j = j+100; }
+  k = i - 4;
+    if (k<1)  { k = k+100; }
+  l = i - 5;
+    if (l<1)  { l = l+100; }
+  m = i - 6;
+    if (m<1)  { m = m+100; }
+    
   // Proportional
-  //roll = gyro_y;
-  roll = (gyro_1 + gyro_2)/2;   // averaging against instantaneous errors
-  rollProp = roll - rollTarget;
+  rollProp = (gyro[i]+gyro[b])/2 - rollTarget;
 
   // Integrated
-  rollInt = (gyro_1+gyro_2+gyro_3+gyro_4+gyro_5 - rollTarget*5) * dataTime*5;
+  rollInt = (gyro[i]+gyro[n]+gyro[j]+gyro[k]+gyro[l]+gyro[m]-rollTarget*6) * dataTime*5;
 
-  // Derivative
-  rollDer = (gyro_1 - gyro_3)/(dataTime*3);  // just enough to average instantaneous errors
+  // Derivative       - just enough points to average instantaneous errors
+  rollDer = ((gyro[i]-gyro[b])/(dataTime) + (gyro[i]-gyro[n])/(dataTime*2))/2;
   
   if (abs(rollProp)>=rollTol) {
         v = 90 + Kp*rollProp + Ki*rollInt + Kd*rollDer;
@@ -92,22 +119,22 @@ void loop() {
         Serial.print  (Kd*rollDer);
         Serial.print  ("  =  ");
         Serial.println(v);
+        
   }
 
-  // simulates changing gyro data - cycles through positions
-  gyro_0  = gyro_10;    // temporary storage variable
-  gyro_10 = gyro_9;
-  gyro_9 = gyro_8;
-  gyro_8 = gyro_7;
-  gyro_7 = gyro_6;
-  gyro_6 = gyro_5;
-  gyro_5 = gyro_4;
-  gyro_4 = gyro_3;
-  gyro_3 = gyro_2;
-  gyro_2 = gyro_1;
-  gyro_1 = gyro_0;
-  delay(300);
-
+  /*if (myFile) {
+    Serial.print("Writing to file...");
+    myFile.println(gyro[i]);
+    Serial.println("done!");
+    delay(2000);
+  }*/
+  /*myFile.println(gyro[i]);*/
   
-  delay(20);           // simulates time spent running other code
+  timestep = millis() - time0;
+  Serial.print("timestep = ");
+  Serial.println(timestep);
+  delay(140);
+  time0 = millis();
+  
+  delay(loopDelay);           // adjusted for desired dataTime
 }
