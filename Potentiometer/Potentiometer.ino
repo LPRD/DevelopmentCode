5b1a8df83ae8152bb97b6e137f6e492a81d8df82
Pierre Abillama <abill001@umn.edu>
2016-11-04 16:09:12 -0500

    Modified version w/ 2 pressure pins

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -1,14 +1,17 @@
 //Define the period of data collection here
 #define PERIOD 1000
 
-//Define Analog Pin used
-int potPin = 1;
+//Define Analog Pins used
+int pressurePinKerosene = 0;
+int pressurePinOxygen = 1;
+int potPin = 2;
 unsigned long int last_run_time = 0;
 void setup() 
 {
   Serial.begin (9600);
-  pinMode (potPin, OUTPUT);
-
+  pinMode (potPin, INPUT);
+  pinMode (pressurePinOxygen, INPUT);
+  pinMode (pressurePinKerosene, INPUT) ;
 }
 
 void loop() 
@@ -16,8 +19,10 @@ void loop()
   if (millis() > last_run_time + PERIOD) 
   {
   last_run_time = millis ();
-  Serial.print ("Vout: ");
-  Serial.println (getVout(potPin));
+  Serial.print ("Pressure Kerosene ");
+  Serial.println (getPressure (pressurePinKerosene));
+  Serial.print ("Pressure Oxygen ");
+  Serial.println (getPressure (pressurePinOxygen));
   Serial.print ("Angle (Degrees): ");
   Serial.println (getAngle(potPin));
   }
@@ -29,9 +34,9 @@ int getDAC (int potPin)
   return DAC;
 }
 
-double getVout (int potPin)
+double getVout (int pin)
 {
- double Vout = getDAC(potPin) * 5 / 1024. ;
+ double Vout = getDAC(pin) * 5 / 1024. ;
  return Vout;
 }
 
@@ -40,3 +45,10 @@ double getAngle (int potPin)
   double angle = getVout(potPin) * 180 / 3.13;
   return angle; 
 }
+
+
+double getPressure (int pressurePin) 
+{
+    double PSIG = getVout(pressurePin) * 246.58 - 118.33;
+    return PSIG;
+} 
